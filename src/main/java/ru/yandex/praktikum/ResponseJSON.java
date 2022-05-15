b1a499bca2b54bd81c6f7354643fcaa99deecc30
Mertuzela <102688891+Mertuzela@users.noreply.github.com>
2022-05-15 16:01:07 +0300

    Update ResponseJSON.java

--- src/main/java/ru/yandex/praktikum/ResponseJSON.java
+++ src/main/java/ru/yandex/praktikum/ResponseJSON.java
@@ -1,2 +1,153 @@
-package ru.yandex.praktikum;public class ResponseJSON {
+package ru.yandex.praktikum;
+
+import java.util.ArrayList;
+
+public class ResponseJSON {
+    private ArrayList<String[]> orders;
+    private int id;
+    private String courierId;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String metroStation;
+    private String phone;
+    private String rentTime;
+    private String deliveryDate;
+    private int track;
+    private String color;
+    private String comment;
+    private String createdAt;
+    private String updatedAt;
+    private int status;
+
+    public ResponseJSON() {
+    }
+
+    public ArrayList<String[]> getOrders() {
+        return this.orders;
+    }
+
+    public void setOrders(ArrayList<String[]> orders) {
+        this.orders = orders;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCourierId() {
+        return this.courierId;
+    }
+
+    public void setCourierId(String courierId) {
+        this.courierId = courierId;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMetroStation() {
+        return this.metroStation;
+    }
+
+    public void setMetroStation(String metroStation) {
+        this.metroStation = metroStation;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRentTime() {
+        return this.rentTime;
+    }
+
+    public void setRentTime(String rentTime) {
+        this.rentTime = rentTime;
+    }
+
+    public String getDeliveryDate() {
+        return this.deliveryDate;
+    }
+
+    public void setDeliveryDate(String deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public int getTrack() {
+        return this.track;
+    }
+
+    public void setTrack(int track) {
+        this.track = track;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getComment() {
+        return this.comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return this.updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public int getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
