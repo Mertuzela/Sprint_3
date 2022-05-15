9f1374fc77e2982b64960db154714e9a6d624710
Mertuzela <102688891+Mertuzela@users.noreply.github.com>
2022-05-15 16:10:42 +0300

    Update FullResponse.java

--- src/main/java/ru/yandex/praktikum/FullResponse.java
+++ src/main/java/ru/yandex/praktikum/FullResponse.java
@@ -3,17 +3,13 @@ package ru.yandex.praktikum;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
-@JsonIgnoreProperties({"track"})
+@JsonIgnoreProperties("track")
 public class FullResponse {
     private List<ResponseJSON> orders;
 
-    public FullResponse() {
-    }
-
     public List<ResponseJSON> getOrders() {
-        return this.orders;
+        return orders;
     }
-
     public void setOrders(List<ResponseJSON> orders) {
         this.orders = orders;
     }
