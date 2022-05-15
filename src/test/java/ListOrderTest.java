a72de782cc257acb25766028bfcc2d1ca67d7527
Mertuzela <102688891+Mertuzela@users.noreply.github.com>
2022-05-15 15:57:56 +0300

    Update ListOrderTest.java

--- src/test/java/ListOrderTest.java
+++ src/test/java/ListOrderTest.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class ListOrderTest {
+import courier.RestAssuredClient;
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.Response;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+import ru.yandex.praktikum.FullResponse;
+
+public class ListOrderTest extends RestAssuredClient {
+    public ListOrderTest() {
+    }
+
+    @Test
+    @DisplayName("Get order list")
+    @Description("Check for get order list and deserialization in JAVA class")
+    public void getOrderList() {
+        FullResponse response = (FullResponse)((Response)this.reqSpec.when().post("/orders", new Object[0])).body().as(FullResponse.class);
+        MatcherAssert.assertThat(response, CoreMatchers.notNullValue());
+    }
 }
