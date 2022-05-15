41e16039386c726a5c2658ffedb236f9a12364f0
Mertuzela <102688891+Mertuzela@users.noreply.github.com>
2022-05-15 15:54:55 +0300

    Update CourierCreateTest.java

--- src/test/java/CourierCreateTest.java
+++ src/test/java/CourierCreateTest.java
@@ -1,40 +1,45 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 import courier.Courier;
 import courier.CourierClient;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
+import courier.CourierCredentials;
+import courier.RestAssuredClient;
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.equalTo;
-
-public class CourierCreateTest {
+public class CourierCreateTest extends RestAssuredClient {
     private CourierClient courierClient;
+    private int courierId;
+
+    public CourierCreateTest() {
+    }
 
     @Before
     public void setUp() {
-        new CourierClient();
-        RestAssured.baseURI = "http://qa-scooter.praktikum-services.ru/";
+        this.courierClient = new CourierClient();
     }
 
-    @Test //Полный флоу создания
-    public void createCourierFull () {
+    @After
+    public void teardown() {
+        this.courierClient.delete(this.courierId);
+    }
+
+    @Test
+    @DisplayName("Create courier")
+    @Description("Check for create courier with valid data and get courier ID")
+    public void createCourierFull() {
         Courier courier = Courier.getRandom();
-        boolean created = courierClient.create(courier);
-
-    }
-    @Test // Пустые поля запроса
-    public void createCourierEmptyBody () {
-        CourierCreate courierCreate = new CourierCreate();
-        Response response =
-        given()
-                .header("Content-type", "application/json")
-                .and()
-                .body(courierCreate)
-                .when()
-                .post("/api/v1/courier");
-                response.then().assertThat().statusCode(400)
-                        .and()
-                        .body(equalTo("{\"code\":400,\"message\":\"Недостаточно данных для создания учетной записи\"}"));
+        boolean created = (new CourierClient()).create(courier);
+        CourierCredentials creds = CourierCredentials.from(courier);
+        this.courierId = this.courierClient.login(creds);
+        Assert.assertTrue(created);
+        Assert.assertNotEquals(0L, (long)this.courierId);
     }
 }
