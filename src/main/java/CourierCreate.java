c097c473df721cc1b4c753909ca4a28a19c16a47
Mertuzela <102688891+Mertuzela@users.noreply.github.com>
2022-05-15 16:10:00 +0300

    Update CourierCreate.java

--- src/main/java/CourierCreate.java
+++ src/main/java/CourierCreate.java
@@ -2,16 +2,12 @@ public class CourierCreate {
     private String login;
     private String password;
 
-    public String getLogin() {
-        return this.login;
-    }
+    public String getLogin() {return login;}
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
+    public void setLogin(String login) {this.login = login;}
 
     public String getPassword() {
-        return this.password;
+        return password;
     }
 
     public void setPassword(String password) {
