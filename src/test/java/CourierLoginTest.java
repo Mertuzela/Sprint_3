77c8d6439a8ac4e3b22477cc748403a648ec2440
Mertuzela <102688891+Mertuzela@users.noreply.github.com>
2022-05-15 15:55:39 +0300

    Update CourierLoginTest.java

--- src/test/java/CourierLoginTest.java
+++ src/test/java/CourierLoginTest.java
@@ -1,76 +1,42 @@
-import io.restassured.RestAssured;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import courier.RestAssuredClient;
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.Response;
-import org.junit.Before;
+import io.restassured.response.ValidatableResponse;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.notNullValue;
-
-public class CourierLoginTest {
-
-    @Before
-    public void setUp() {
-        RestAssured.baseURI = "http://qa-scooter.praktikum-services.ru/";
+public class CourierLoginTest extends RestAssuredClient {
+    public CourierLoginTest() {
     }
-//    курьер может авторизоваться;+
-//    для авторизации нужно передать все обязательные поля;+
-//    система вернёт ошибку, если неправильно указать логин или пароль;+
-//    если какого-то поля нет, запрос возвращает ошибку;
-//    если авторизоваться под несуществующим пользователем, запрос возвращает ошибку;
-//    успешный запрос возвращает id.+
 
-    @Test // Авторизация
+    @Test
+    @DisplayName("Login courier")
+    @Description("Check for login courier in system with valid data")
     public void loginCourier() {
-        CourierCreate courierCreate = new CourierCreate("ninja","1234");
-        Response response =
-                given()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(courierCreate)
-                        .when()
-                        .post("/api/v1/courier/login");
-        response.then().assertThat().body("id", notNullValue())
-                .and()
-                .statusCode(200);
+        CourierCreate courierCreate = new CourierCreate("ninja", "1234");
+        ((ValidatableResponse)((ValidatableResponse)((ValidatableResponse)((ValidatableResponse)((ValidatableResponse)((Response)this.reqSpec.body(courierCreate).when().post("/courier/login", new Object[0])).then()).log().all()).assertThat()).body("id", CoreMatchers.notNullValue(), new Object[0])).and()).statusCode(200);
     }
 
-    @Test // Авторизация пустое поле
-    public void loginCourierEmptyBody() {
-        CourierCreate courierCreate = new CourierCreate();
-        Response response =
-                given()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(courierCreate)
-                        .when()
-                        .post("/api/v1/courier/login");
-        response.then().assertThat().statusCode(400);
-    }
-
-    @Test // Авторизация неверный логин
+    @Test
+    @DisplayName("Login courier with incorrect login")
+    @Description("Check for login courier in system with incorrect login")
     public void loginCourierIncorrectLogin() {
-        CourierCreate courierCreate = new CourierCreate("ninj1a","1234");
-        Response response =
-                given()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(courierCreate)
-                        .when()
-                        .post("/api/v1/courier/login");
-        response.then().assertThat().statusCode(404);
+        CourierCreate courierCreate = new CourierCreate("ninj1a", "1234");
+        ((ValidatableResponse)((ValidatableResponse)((ValidatableResponse)((ValidatableResponse)((Response)this.reqSpec.body(courierCreate).when().post("/courier/login", new Object[0])).then()).log().all()).assertThat()).statusCode(404)).body(CoreMatchers.equalTo("{\"code\":404,\"message\":\"Учетная запись не найдена\"}"), new Matcher[0]);
     }
 
-    @Test // Авторизация неверный пароль
+    @Test
+    @DisplayName("Login courier with incorrect/empty password")
+    @Description("Check for login courier in system with incorrect/empty password")
     public void loginCourierIncorrectPassword() {
-        CourierCreate courierCreate = new CourierCreate("ninja","12341234");
-        Response response =
-                given()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(courierCreate)
-                        .when()
-                        .post("/api/v1/courier/login");
-        response.then().assertThat().statusCode(404);
+        CourierCreate courierCreate = new CourierCreate("ninja", "");
+        ((ValidatableResponse)((ValidatableResponse)((ValidatableResponse)((ValidatableResponse)((Response)this.reqSpec.body(courierCreate).when().post("/courier/login", new Object[0])).then()).log().all()).assertThat()).statusCode(400)).body(CoreMatchers.equalTo("{\"code\":400,\"message\":\"Недостаточно данных для входа\"}"), new Matcher[0]);
     }
 }
