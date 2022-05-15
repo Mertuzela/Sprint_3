d0de36a882f8eb1edca6852aa4efd4626ec09372
Mertuzela <102688891+Mertuzela@users.noreply.github.com>
2022-05-15 15:59:19 +0300

    Update OrderData.java

--- src/main/java/ru/yandex/praktikum/OrderData.java
+++ src/main/java/ru/yandex/praktikum/OrderData.java
@@ -1,2 +1,25 @@
-package ru.yandex.praktikum;public class OrderData {
+package ru.yandex.praktikum;
+
+import courier.RestAssuredClient;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
+public class OrderData extends RestAssuredClient {
+    private int track;
+
+    public OrderData() {
+    }
+
+    public int orderCreate(String color) {
+        this.track = (Integer)((ValidatableResponse)((ValidatableResponse)((ValidatableResponse)((ValidatableResponse)((Response)this.reqSpec.body(color).when().post("/orders", new Object[0])).then()).log().all()).assertThat()).statusCode(201)).extract().path("track", new String[0]);
+        return this.track;
+    }
+
+    public int orderTrack() {
+        return this.track;
+    }
+
+    public void getOrderData() {
+        ((ValidatableResponse)((ValidatableResponse)((ValidatableResponse)((Response)this.reqSpec.when().get("/orders/track?t=" + this.track, new Object[0])).then()).log().all()).assertThat()).statusCode(200);
+    }
 }
